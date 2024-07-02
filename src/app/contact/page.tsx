de35d370cb7b2d1c18bf28db2436be96203a6f74
mohamed abd elkhalk <01147018091.m@gmial.com>
2024-07-02 23:01:05 +0300

    fix contact Ui

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -62,7 +62,7 @@ export default function Contact() {
             </form>
           </div>
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            <ul>
+            <ul className=" flex flex-col gap-3">
               {info.map((item, index) => (
                 <li key={index} className="flex items-center gap-6">
                   <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
